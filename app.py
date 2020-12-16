25944ad01116d957c298e2d6df3210e7bb167d9c
Alex40144 <giddyraccoon@gmail.com>
2020-12-16 17:28:53 +0000

    button works

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/style.css')
-def stylesheet():
-    return 
+@app.route('/play')
+def play():
+    return render_template('play.html')
+
 
 
 if __name__ == "__main__":
