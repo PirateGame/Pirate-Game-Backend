16ec027e817ed489c4af511516dc3318815d64e6
Alex40144 <giddyraccoon@gmail.com>
2020-12-16 17:24:53 +0000

    button

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import database
 
 app = Flask(__name__)
 
---
+++ database.py
@@ -0,0 +1,4 @@
+import pymongo
+
+#use this file for interacting with the database.
+#probably don't need  use auth, just games and game state.
