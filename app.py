52b1eca938639bcb6432709f493d0109be8bb9ff
Alex40144 <giddyraccoon@gmail.com>
2020-12-16 17:15:42 +0000

    working website

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request, send_file, jsonify
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/style.css')
+def stylesheet():
+    return 
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
