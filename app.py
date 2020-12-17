86c08011b45a26860073c198f9df5800a145cdf4
Alex40144 <giddyraccoon@gmail.com>
2020-12-17 18:41:46 +0000

    🔥

--- app.py
+++ app.py
@@ -12,6 +12,11 @@ def play():
     return render_template('play.html')
 
 
+@app.route('/host')
+def host():
+    return render_template('host.html')
+
+
 
 if __name__ == "__main__":
-    app.run(debug=False, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0")
