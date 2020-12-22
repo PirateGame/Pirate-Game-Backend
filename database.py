e4ed313a884c3f7571ee4516631c1af39cf6d46e
Thomas Wood <33393308+ThomasAWood@users.noreply.github.com>
2020-12-22 10:22:57 +0000

    Added connection script to database.py

--- database.py
+++ database.py
@@ -1,4 +1,8 @@
 import pymongo
 
+
+client = pymongo.MongoClient("mongodb+srv://user123:bjADXameSKXGGD76@cluster0.pcoof.mongodb.net/pirategame?retryWrites=true&w=majority")
+db = client.test
+
 #use this file for interacting with the database.
 #probably don't need  use auth, just games and game state.
