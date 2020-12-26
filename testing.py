23060b4f9594bfa03291e3e58fa7c80c15e51eea
portaljumper007 <enchantedportal007@gmail.com>
2020-12-26 16:50:49 +0000

    test grid serialise

---
+++ testing.py
@@ -0,0 +1,13 @@
+import grid
+gridTemplate = grid.grid((5,5))
+exampleGrid = gridTemplate.build()
+
+JSON = []
+
+gridHeight = len(exampleGrid)
+gridWidth = len(exampleGrid[0])
+for y in range(gridHeight):
+    for x in range(gridWidth):
+        JSON.append({"x":x, "y":y, "w":1, "h":1, "id":((y+1) * gridWidth) + x, "content":""})
+
+print(JSON)
