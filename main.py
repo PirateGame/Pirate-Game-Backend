19e488fbd96d70cc1e372169bea1e7b152615a61
Alex40144 <giddyraccoon@gmail.com>
2021-02-11 18:57:12 +0000

    new package

--- main.py
+++ main.py
@@ -1545,6 +1545,7 @@ def sendPlayerListToClients(gameName):
         emit("playerList", data, room=gameName)
     
     clientList = listClients(gameName)
+    print(clientList)
     toSend = []
     for clientName,about in clientList.items():
         text = str(about["type"]) + ": " + str(clientName)
